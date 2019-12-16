f72cc2ee13fd7fbfb74159debff6802c7d30c057
Daniel Florez <danielflorez111@hotmail.com>
2019-12-16 00:09:31 -0500

    Adding body-parser and some routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.json('Hello World');
-})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/user', (req, res) => {
+    res.json('get user');
+});
+
+app.post('/user', (req, res) => {
+    let body = req.body;
+    res.json(body);
+});
+
+app.put('/user/:id', (req, res) => {
+    const id = req.params.id
+    res.json({ id });
+});
+
+app.delete('/user/:id', (req, res) => {
+    const id = req.params.id
+    res.json({ id });
+});
 
 app.listen(3000, () => {
-    console.log('Escuchando puerto: ', 3000);
+    console.log('Listening on port: ', 3000);
 });
